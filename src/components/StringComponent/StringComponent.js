71e2cb4860c27e8e16e3501154b4cbc895dec3ec
Cibermag <body1945@ukr.net>
2022-08-13 17:32:38 +0300

    Homework3react

--- src/components/StringComponent/StringComponent.js
+++ src/components/StringComponent/StringComponent.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
 export default class StringComponent extends Component {
-    state = {
-        text: this.props.text
-    }
+    // state = {
+    //     text: this.props.text
+    // }
     render() {
         return (
-            <div>{this.state.text}</div>
+            <div>{this.props.text}</div> //if use state we can this.state.text
         )
     }
 }
